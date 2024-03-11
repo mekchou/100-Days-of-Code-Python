3c27836b78bb2c186b06c51ff427d9534523bedd
mek <mek.chou@gmail.com>
2024-03-11 19:25:22 -0400

    challenge1

---
+++ D7Project.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+wordList = ["cat", "rainbow", "dragonball"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+chosenWord = random.choice(wordList) 
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = ""
+while not (len(guess) == 1 and guess in string.ascii_letters):
+  guess = input("guess a letter\n").lower()
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for num in range(len(chosenWord)):
+  if chosenWord[num] == guess:
+    print("Right")
+  else:
+    print("Wrong")
+print(f"the word is {chosenWord}")
+print(f"letter entered is {guess}")
