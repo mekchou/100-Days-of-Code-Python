716b20c820e65c90481f02408c7f6a0884726dc2
mek <mek.chou@gmail.com>
2024-03-12 20:02:24 -0400

    challenge3

--- D7Project.py
+++ D7Project.py
@@ -5,23 +5,25 @@ wordList = ["cat", "rainbow", "dragonball"]
 chosenWord = random.choice(wordList) 
 guess = ""
 display = []
-
+endOfGame = False
 for letter in chosenWord:
   display.append("_")
 
-while not (len(guess) == 1 and guess in string.ascii_letters):
-  guess = input("guess a letter\n").lower()
-
-
-
-for num in range(len(chosenWord)):
-  if chosenWord[num] == guess:
-    print("Right")
-    display[num] = guess
+while not endOfGame:
+  while not (len(guess) == 1 and guess in string.ascii_letters):
+    guess = input("guess a letter\n").lower()
+  for num in range(len(chosenWord)):
+    if chosenWord[num] == guess:
+      # print("Right")
+      display[num] = guess
+    # else:
+      # print("Wrong")
+  print(display)
+  if "_" not in display:
+    endOfGame = True
+    print("you win!")
   else:
-    print("Wrong")
+    guess = ""
 
-
-print(f"display is \n {display}")
 print(f"the word is {chosenWord}")
-print(f"letter entered is {guess}")
+# print(f"letter entered is {guess}")
