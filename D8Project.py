94bc25a2ddd6e642582e502b6d972a09817692a6
mek <mek.chou@gmail.com>
2024-03-14 20:38:25 -0400

    part2, include invalid mode and non-alphabet handling

--- D8Project.py
+++ D8Project.py
@@ -4,20 +4,39 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+# encrypt
 def encrypt(plainText, shiftAmount):
   cipherText = ""
   # loop to encode each letter in plainText
   for letter in plainText:
-    plainIndex = alphabet.index(letter)
-    if (plainIndex + shiftAmount) > (len(alphabet) - 1):
-      cipherIndex = plainIndex + shiftAmount - 26
+    if letter in alphabet:
+      plainIndex = alphabet.index(letter)
+      if (plainIndex + shiftAmount) > (len(alphabet) - 1):
+        cipherIndex = plainIndex + shiftAmount - 26
+      else:
+        cipherIndex = plainIndex + shiftAmount
+      cipherText += alphabet[cipherIndex]
     else:
-      cipherIndex = plainIndex + shiftAmount
-    cipherText += alphabet[cipherIndex]
+      cipherText += letter
   print(f"Here's the encoded result: {cipherText}")
 
+# decrypt
+def decrypt(plainText, shiftAmount):
+  cipherText = ""
+  # loop to encode each letter in plainText
+  for letter in plainText:
+    plainIndex = alphabet.index(letter)
+    if (plainIndex - shiftAmount) < 0:
+      cipherIndex = plainIndex - shiftAmount + 26
+    else:
+      cipherIndex = plainIndex - shiftAmount
+    cipherText += alphabet[cipherIndex]
+  print(f"Here's the decoded result: {cipherText}")
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
 if direction == "encode":
   encrypt(text, shift)
+elif direction == "decode":
+  decrypt(text, shift)
+else:
+  print("invalid mode")
