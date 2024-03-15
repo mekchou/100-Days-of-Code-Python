bbd2716a0d00f858097ea8074598c2011ecaa2f4
mek <mek.chou@gmail.com>
2024-03-14 21:01:02 -0400

    part3

--- D8Project.py
+++ D8Project.py
@@ -4,39 +4,28 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# encrypt
-def encrypt(plainText, shiftAmount):
+# combine encrypt and decrypt into caesar
+def caesar(plainText, shiftAmount, directionMode):
   cipherText = ""
   # loop to encode each letter in plainText
+  if direction == "decode":
+    shiftAmount *= -1
   for letter in plainText:
     if letter in alphabet:
       plainIndex = alphabet.index(letter)
       if (plainIndex + shiftAmount) > (len(alphabet) - 1):
         cipherIndex = plainIndex + shiftAmount - 26
+      elif (plainIndex + shiftAmount) < 0:
+        cipherIndex = plainIndex + shiftAmount + 26
       else:
         cipherIndex = plainIndex + shiftAmount
       cipherText += alphabet[cipherIndex]
     else:
       cipherText += letter
-  print(f"Here's the encoded result: {cipherText}")
-
-# decrypt
-def decrypt(plainText, shiftAmount):
-  cipherText = ""
-  # loop to encode each letter in plainText
-  for letter in plainText:
-    plainIndex = alphabet.index(letter)
-    if (plainIndex - shiftAmount) < 0:
-      cipherIndex = plainIndex - shiftAmount + 26
-    else:
-      cipherIndex = plainIndex - shiftAmount
-    cipherText += alphabet[cipherIndex]
-  print(f"Here's the decoded result: {cipherText}")
+  print(f"Here's the {directionMode} result: {cipherText}")
 
 
-if direction == "encode":
-  encrypt(text, shift)
-elif direction == "decode":
-  decrypt(text, shift)
+if direction in ["encode", "decode"]:
+  caesar(text, shift, direction)
 else:
   print("invalid mode")
