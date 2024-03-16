96c3bce98922f14760fbfa2b22c8fdd86b11bb81
mek <mek.chou@gmail.com>
2024-03-16 15:51:49 -0400

    core functions

--- D10Project.py
+++ D10Project.py
@@ -1,15 +1,33 @@
 
 # plus function
-def plus(m, n):
+def add(m, n):
   return m + n
 # minus function
-def minus(m, n):
+def substract(m, n):
   return m - n
 
 # multiple function
-def multiple(m, n):
-  return m ** n
+def multiply(m, n):
+  return m * n
 
 # divide function
 def divide(m, n):
   return m / n
+
+operations = {
+  "+": add,
+  "-": substract,
+  "*": multiply,
+  "/": divide
+}
+
+# function = operations["+"]
+# print(function(2,3))
+
+num1 = int(input("What's the fist number?: "))
+num2 = int(input("What's the second number?: "))
+for symbol in operations:
+  print(symbol)
+operationSymbol = input("Pick an operation from the line above: ")
+answer = operations[operationSymbol](num1, num2)
+print(f"{num1} {operationSymbol} {num2} = {answer}")
