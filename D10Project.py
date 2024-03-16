38c709b5c723b93818f461fac736cb5a1ecaf86f
mek <mek.chou@gmail.com>
2024-03-16 11:22:31 -0400

    +-*/ function

---
+++ D10Project.py
@@ -0,0 +1,15 @@
+
+# plus function
+def plus(m, n):
+  return m + n
+# minus function
+def minus(m, n):
+  return m - n
+
+# multiple function
+def multiple(m, n):
+  return m ** n
+
+# divide function
+def divide(m, n):
+  return m / n
