c521bbea6bd9c493fb41816ebbf0137ac6e17868
Mek <mek.chou@gmail.com>
2024-03-24 13:52:58 -0400

    part3

---
+++ D17/quiz_brain.py
@@ -0,0 +1,18 @@
+class QuizBrain:
+    def __init__(self, q_list) -> None:
+        self.question_number = 0
+        self.question_list = q_list
+
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        u_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        
+
+
+# TODO: asking the questions
+
+# TODO: checking if the answer was correct
+
+# TODO: checking if we're the end of the quiz
