432cd405e99825e27751eb4f2d183c44478b6e69
Mek <mek.chou@gmail.com>
2024-03-25 11:56:26 -0400

    challenge4 add input function

--- D18/main.py
+++ D18/main.py
@@ -1,15 +1,24 @@
 from turtle import Turtle, Screen
+import random
 
+def random_color_hex():
+    color = ''.join([random.choice('0123456789ABCDEF') for _ in range(6)])
+    return '#' + color
 
 tim = Turtle()
+tim.shape("classic")
 
-tim.shape("turtle")
-tim.color("red")
-for num in range(50):
-    tim.forward(10)
-    if num % 2 == 0:
-        tim.up()
-    else:
-        tim.down()
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    tim.pencolor(random_color_hex())
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+    
+    
+sides = int(input("Input # of sides you want to print up to: "))
+    
+for shape in range(3, sides + 1):
+    draw_shape(shape)
 
 input("Press Enter to exit...")
