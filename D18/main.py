c26afdbe23d25deaff778ce941996233b3bb9717
Mek <mek.chou@gmail.com>
2024-03-26 20:13:03 -0400

    challenge4

--- D18/main.py
+++ D18/main.py
@@ -7,18 +7,22 @@ def random_color_hex():
 
 tim = Turtle()
 tim.shape("classic")
+tim.pensize(5)
+tim.speed(8)
+# def draw_shape(num_sides):
+    # angle = 360 / num_sides
+    # for _ in range(num_sides):
+        # tim.forward(100)
+        # tim.right(angle)
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
+    
+# sides = int(input("Input # of sides you want to print up to: "))
+    
+for _ in range(100):
     tim.pencolor(random_color_hex())
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-    
-    
-sides = int(input("Input # of sides you want to print up to: "))
-    
-for shape in range(3, sides + 1):
-    draw_shape(shape)
+    steps = 10
+    angle = random.randint(1,4) * 90
+    tim.right(angle)
+    tim.forward(steps)
 
 input("Press Enter to exit...")
