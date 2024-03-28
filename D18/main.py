5b7e0cb64ac589138c202fede734994d935124e4
Mek <mek.chou@gmail.com>
2024-03-27 21:07:32 -0400

    first version

---
+++ D18/main.py
@@ -0,0 +1,24 @@
+import colorgram as cg
+
+
+def rbg_list(color_obj):
+    rgb = []
+    length = len(color_obj)
+    for num in range(length):
+        rgb.append(color_obj[num].rgb)
+    
+    rgblist = [None for _ in range(len(rgb))]
+    
+    for num in range(len(rgb)):
+        # for ind in range(3):
+        rgblist[num] = (rgb[num][0], rgb[num][1], rgb[num][2])
+        
+    
+    return rgblist
+
+colors = cg.extract(r"V:\100-Days-of-Code-Python\data\HirstSpot.jpg",40)
+
+print(rbg_list(colors))
+
+
+# print(colors)
