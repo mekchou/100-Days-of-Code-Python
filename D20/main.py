823df0ccb3f6f9e5bd36f5f73cac80364b5e4a15
Mek <mek.chou@gmail.com>
2024-03-30 11:57:47 -0400

    initial set up

---
+++ D20/main.py
@@ -0,0 +1,34 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+# TODO: create a snake body
+
+
+# TODO: move the snake
+
+
+# TODO: control the snake
+
+
+
+
+# TODO: detect collision with food
+
+
+# TODO: create a scoreboard
+
+
+# TODO: detect collision with wall
+
+
+# TODO: detect collision with tail
+
+
+
+screen.exitonclick()
