4e3c006a6d600aa671f338ed0319b16b74615efe
Mek <mek.chou@gmail.com>
2024-03-31 10:48:47 -0400

    move snake

--- D20/main.py
+++ D20/main.py
@@ -6,24 +6,31 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 
-
+snake = []
+game_is_on = True
 
 # TODO: create a snake body
 def snake_body(snake_len):
     starting_positions = []
     for turtle_index in range(snake_len):
-        new_turtle = Turtle(shape = "square")
-        new_turtle.color("white")
-        new_turtle.pensize(10)
+        new_segment = Turtle(shape = "square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.speed(0)
+        # new_segment.pensize(10)
         starting_positions.append((0 - turtle_index * 20, 0))
         # print(starting_positions[turtle_index])
-        # new_turtle.teleport(starting_positions[turtle_index])
-        new_turtle.goto(starting_positions[turtle_index])
-        new_turtle.penup
+        # new_segment.teleport(starting_positions[turtle_index])
+        new_segment.goto(starting_positions[turtle_index])
+        snake.append(new_segment)
 
 
 
 # TODO: move the snake
+def move_snake(snake):
+    while game_is_on:
+        for seg in snake:
+            seg.forward(20)
 
 
 # TODO: control the snake
@@ -43,5 +50,6 @@ def snake_body(snake_len):
 # TODO: detect collision with tail
 
 snake_body(3)
+move_snake(snake)
 
 screen.exitonclick()
