7bcfe07668bcd9da91eb660aba4803be8949faa2
Mek <mek.chou@gmail.com>
2024-03-31 11:10:50 -0400

    move each snake to position of prior segment

--- D20/main.py
+++ D20/main.py
@@ -32,8 +32,11 @@ def move_snake(snake):
     while game_is_on:
         screen.update()
         time.sleep(0.1)
-        for seg in snake:
-            seg.forward(20)
+        for seg_num in range(len(snake) - 1, 0, -1): 
+            new_x = snake[seg_num - 1].xcor()
+            new_y = snake[seg_num - 1].ycor()
+            snake[seg_num].goto(new_x, new_y)
+        snake[0].forward(20)
 
 
 # TODO: control the snake
