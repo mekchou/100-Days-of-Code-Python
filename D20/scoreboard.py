4e44a0df1f8d588555add831bf1c5a92e53bc8db
Mek <mek.chou@gmail.com>
2024-04-02 19:31:47 -0400

    apply add score and print score function

---
+++ D20/scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.current_score = 0
+        self.shape("blank")
+        self.penup()
+        self.color("white")
+        self.speed(0)
+        self.goto(0, 280)
+        self.print_score()
+
+        
+    def add_score(self):
+        self.current_score += 1
+    
+    def print_score(self):
+        self.clear()
+        self.write(f"Score: {self.current_score}", font = ("Arial", 14, "normal"), align = "center")
