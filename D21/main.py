086c8888764585df29a55049461b84f401a0f402
Mek <mek.chou@gmail.com>
2024-04-02 20:45:45 -0400

    add todo

---
+++ D21/main.py
@@ -0,0 +1,23 @@
+
+
+
+
+# TODO: set up main screen
+
+# TODO: create paddle class that responds to key press
+
+# TODO: create 2 paddles
+
+
+# TODO: create ball class including ball move
+
+# TODO: add ball bouncing logic
+
+# TODO: detect ball collisions with paddle
+
+# TODO: detect ball goes out of bound
+
+# TODO: create scoreboard class
+
+
+# TODO: 
