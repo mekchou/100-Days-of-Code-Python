a254216d70bc3ccf51b402afeec53bbf66a630ff
Mek <mek.chou@gmail.com>
2024-04-03 20:12:12 -0400

    main screen

--- D21/main.py
+++ D21/main.py
@@ -1,3 +1,24 @@
+from turtle import Screen, Turtle
+# from snake import Snake
+# from food import Food
+# from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width = 1200, height = 800)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+screen.listen()
+
+# snake = Snake()
+# food = Food()
+# scoreboard = Scoreboard()
+
+# screen.onkeypress(key = "Up", fun = snake.up)
+# screen.onkeypress(key = "Down", fun = snake.down)
+# screen.onkeypress(key = "Left", fun = snake.left)
+# screen.onkeypress(key = "Right", fun = snake.right)
 
 
 
