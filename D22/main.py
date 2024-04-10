aa878d0cf45eae9939c14510c58dc72af7b73799
Kerry Wang <mek.chou@gmail.com>
2024-04-10 18:38:39 -0400

    test basic player movement in main

--- D22/main.py
+++ D22/main.py
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkeypress(key = "Up", fun = player.move)
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
+screen.exitonclick()
