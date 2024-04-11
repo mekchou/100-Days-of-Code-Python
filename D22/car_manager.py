bca99043021a817273e2b168ac673f0d5fd91147
Kerry Wang <mek.chou@gmail.com>
2024-04-10 20:07:59 -0400

    build car_manager class

--- D22/car_manager.py
+++ D22/car_manager.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+import random as rand
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self, y, level):
+        super().__init__()
+        self.color(rand.choice(COLORS))
+        self.shape("square")
+        self.shapesize(stretch_wid = 1, stretch_len = 2)
+        self.y = y
+        self.level = level
+        self.goto((300, self.y))
+    
+    def move(self):
+        new_x = self.xcor() - (STARTING_MOVE_DISTANCE + MOVE_INCREMENT * (self.level - 1))
+        self.goto((new_x, self.y))
