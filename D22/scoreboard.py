b97628cd1004703f2d86576504dfec4f5ae47013
Kerry Wang <mek.chou@gmail.com>
2024-04-11 13:18:16 -0400

    basic scoreboard

--- D22/scoreboard.py
+++ D22/scoreboard.py
@@ -1,5 +1,22 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.speed(0)
+        self.goto(-210,250)
+        self.print_score()
+    
+    def print_score(self):
+        self.write(f"Level: {self.level}", font = FONT, align = ALIGNMENT)
+        
+    def next_level(self):
+        self.level += 1
+        self.print_score()
