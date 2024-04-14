396ae38eadfae2074030c0afa1a26439540828a9
Mek <mek.chou@gmail.com>
2024-04-14 14:19:09 -0400

    first try

--- D24/main.py
+++ D24/main.py
@@ -6,3 +6,28 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open("D24\Input\Letters\starting_letter.txt") as letters:
+    starting_letter = letters.readlines()
+    # print(starting_letter)
+
+with open(r"D24\Input\Names\invited_names.txt") as invited:
+    invited_names_raw = invited.read()
+    invited_names_list = invited_names_raw.splitlines()
+    # invited_names_raw.replace("/n", "")
+    # print(invited_names_list)
+
+
+# print(starting_letter[0])
+for names in invited_names_list:
+    new_letter = starting_letter[0].replace("[name]", names)
+    
+    
+    # print(new_letter)
+    # for invited_names in starting_letter[1:]:
+        # print(n)
+    with open(f"D24\Output\\ReadyToSend\letter_for_{names}.txt", mode = "w") as letter:
+        letter.write(new_letter)
+        for content in starting_letter[1:]:
+            letter.write(content)
