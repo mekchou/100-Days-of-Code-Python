7a49dbc86e2475f1fecd1f74ac5504936f0b0722
Mek <mek.chou@gmail.com>
2024-04-15 19:57:58 -0400

    create csv that list states to learn

--- D25/main.py
+++ D25/main.py
@@ -10,32 +10,28 @@ turtle.shape(image)
 answers = answer.Answer()
 
 dataframe = pd.read_csv(r"data\50_states.csv")
+all_states = dataframe["state"].to_list()
 
-# game_is_on = True
-# score = 0
 
 while len(answers.all_answers) < 50:
 
     answer_state = screen.textinput(title = "Guess the State", prompt = "What's anothe state's name? Type exit to Exit the game").title()
-    all_states = dataframe["state"].to_list()
 
     # if answer state is one of the states
     if answer_state in all_states:
         state_data = dataframe[dataframe["state"] == answer_state]
         answers.print_answer(state_data["state"].item(), int(state_data["x"].item()), int(state_data["y"].item())) 
-        # score += 1
-        
+        all_states.remove(answer_state)
+        # print(all_states)
     elif answer_state == "Exit":
         break
 
+# states_to_learn.csv
+states_to_learn = pd.DataFrame(all_states, columns = ['state'])
+states_to_learn.to_csv(r"D25\states_to_learn.csv", index = False)
 print(f"Your score is {len(answers.all_answers)}")
 
 
 
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-    
-# turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
-
+# turtle.mainloop()
 # screen.exitonclick()
