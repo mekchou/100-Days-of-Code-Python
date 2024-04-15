690c9a3f3d71719215e51c1f2c1fa47ff3e1388d
Mek <mek.chou@gmail.com>
2024-04-15 19:30:51 -0400

    add while loop to allow multi input and add score count

--- D25/main.py
+++ D25/main.py
@@ -11,15 +11,23 @@ answers = answer.Answer()
 
 dataframe = pd.read_csv(r"data\50_states.csv")
 
+game_is_on = True
+score = 0
 
+while game_is_on:
 
-answer_state = screen.textinput(title = "Guess the State", prompt = "What's anothe state's name?").title()
-all_states = dataframe["state"].to_list()
+    answer_state = screen.textinput(title = "Guess the State", prompt = "What's anothe state's name? Type exit to Exit the game").title()
+    all_states = dataframe["state"].to_list()
 
-# if answer state is one of the states
-if answer_state in all_states:
-    state_data = dataframe[dataframe["state"] == answer_state]
-    answers.print_answer(state_data["state"].item(), int(state_data["x"]), int(state_data["y"])) 
+    # if answer state is one of the states
+    if answer_state in all_states:
+        state_data = dataframe[dataframe["state"] == answer_state]
+        answers.print_answer(state_data["state"].item(), int(state_data["x"]), int(state_data["y"])) 
+        score += 1
+        
+    elif answer_state == "Exit":
+        game_is_on = False
+        print(f"Your score is {score}")
 
 
 
