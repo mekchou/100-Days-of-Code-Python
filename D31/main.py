e59dda46444549f20bd42da5b6fc3a8bcd4b053b
Mek <mek.chou@gmail.com>
2024-04-22 14:42:17 -0400

    step 3 improved version

--- D31/main.py
+++ D31/main.py
@@ -8,7 +8,7 @@ TITLE_FONT_SIZE = 40
 TITLE_FONT_STYLE = "italic"
 WORD_FONT_SIZE = 60
 WORD_FONT_STYLE = "bold"
-timer = None
+current_card = {}
 
 # create new flash cards
 # read csv
@@ -21,27 +21,19 @@ data_dict = dataframe.to_dict(orient="records")
 # pick random french word and translation into flashcard
 
 def next_word():
-    try:
-        window.after_cancel(timer)
-    finally:
-        current_card = rand.choice(data_dict)
-        canvas.itemconfig(card_title, text = "French")
-        canvas.itemconfig(card_word, text = current_card["French"])
-        flip_card(0, current_card)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = rand.choice(data_dict)
+    canvas.itemconfig(card_image, image= card_front)
+    canvas.itemconfig(card_title, text = "French", fill="black")
+    canvas.itemconfig(card_word, text = current_card["French"], fill="black")
+    flip_timer = window.after(3000, func=flip_card)
 
 # flip the card
-def flip_card(count, current_card):
-    global timer
-    timer = window.after(3000,flip_card, count + 1, current_card)
-    if count % 2 == 0:
-        canvas.itemconfig(card_image, image = card_front)
-        canvas.itemconfig(card_title, text= "French")
-        canvas.itemconfig(card_word, text= current_card["French"])
-        # next_word()
-    else:
-        canvas.itemconfig(card_image, image = card_back)
-        canvas.itemconfig(card_title, text= "English")
-        canvas.itemconfig(card_word, text= current_card["English"])
+def flip_card():
+    canvas.itemconfig(card_image, image = card_back)
+    canvas.itemconfig(card_title, text= "English", fill="white")
+    canvas.itemconfig(card_word, text= current_card["English"], fill="white")
 
 
 # UI setup
@@ -49,6 +41,7 @@ window = tk.Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(30000, func=flip_card)
 
 
 canvas = tk.Canvas(width=800, height=526)
