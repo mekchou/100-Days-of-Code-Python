14e3c80c189d541d6ec8cf78b7fe71d8fa87172b
Mek <mek.chou@gmail.com>
2024-04-22 11:53:25 -0400

    move lables to canvas item

--- D31/main.py
+++ D31/main.py
@@ -4,8 +4,8 @@ import random as rand
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT = "Arial"
-LANGUAGE_FONT_SIZE = 40
-LANGUAGE_FONT_STYLE = "italic"
+TITLE_FONT_SIZE = 40
+TITLE_FONT_STYLE = "italic"
 WORD_FONT_SIZE = 60
 WORD_FONT_STYLE = "bold"
 
@@ -21,11 +21,13 @@ data_dict = dataframe.to_dict(orient="records")
 # pick random french word and translation into flashcard
 
 def next_word():
-    new_random_word = rand.choice(data_dict)["French"]
-    word_label.config(text=new_random_word)
+    current_card = rand.choice(data_dict)
+    # new_random_word = current_card["French"]
+    canvas.itemconfig(card_title, text = "French")
+    canvas.itemconfig(card_word, text = current_card["French"])
     # print(new_random_word)
 
-
+# flip the card
 
 
 
@@ -38,13 +40,18 @@ canvas = tk.Canvas(width=800, height=526)
 card_front = tk.PhotoImage(file="images\card_front.png")
 canvas.create_image(400,263,image = card_front)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+card_title = canvas.create_text(400, 150, text= "Title", font=(FONT, TITLE_FONT_SIZE, TITLE_FONT_STYLE))
+card_word = canvas.create_text(400, 263, text= "Word", font=(FONT, WORD_FONT_SIZE, WORD_FONT_STYLE))
+
+
+
 canvas.grid(column=0, row=0, columnspan=2)
 
-language_label = tk.Label(text = "French", font=(FONT, LANGUAGE_FONT_SIZE, LANGUAGE_FONT_STYLE), bg="white")
-language_label.place(x=400, y=150, anchor="center")
+# TITLE_label = tk.Label(text = "French", font=(FONT, TITLE_FONT_SIZE, TITLE_FONT_STYLE), bg="white")
+# TITLE_label.place(x=400, y=150, anchor="center")
 
-word_label = tk.Label(text = "Word", font=(FONT, WORD_FONT_SIZE, WORD_FONT_STYLE), bg="white")
-word_label.place(x=400, y=285, anchor="center", )
+# word_label = tk.Label(text = "Word", font=(FONT, WORD_FONT_SIZE, WORD_FONT_STYLE), bg="white")
+# word_label.place(x=400, y=285, anchor="center", )
 
 right_image = tk.PhotoImage(file=r"images\right.png")
 right_button = tk.Button(image=right_image, highlightthickness=0, command=next_word)
