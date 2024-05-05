2318a7a619a36fa903db645ffc6781fb82056af2
Mek <mek.chou@gmail.com>
2024-05-05 10:51:24 -0400

    save input into google sheet

--- D38/main.py
+++ D38/main.py
@@ -6,12 +6,15 @@ WEIGHT_KG = 65
 HEIGHT_CM = 168
 AGE = 34
 
-
-
 NUTRITION_APP_ID = "63a7ba28"
 NUTRITION_API_KEY = "5222cdd05b53a0e6c04289ab5d5eea03"
 NUTRITION_HOST_DOMAIN = "https://trackapi.nutritionix.com"
 NUTIOTION_ENDPOINT = "/v2/natural/exercise"
+
+SHEETY_URL = "https://api.sheety.co/f57eb1dd3b398c09d11ae905bda32e17/workoutTracking/workouts"
+
+
+
 
 def get_exercise_stats(prompt):
     headers = {
@@ -30,4 +33,17 @@ def get_exercise_stats(prompt):
     
 # get_exercise_stats(input("prompt: "))
 
-# def _add_exercise():
+def add_exercise(exercise, duration, calories):
+    parameters = {
+        "workout":{
+            "date": datetime.now().strftime("%Y/%m/%d"),
+            "time": datetime.now().strftime("%X"),
+            "exercise": exercise,
+            "duration": duration,
+            "calories": calories
+        }
+    }
+    response = requests.post(url = SHEETY_URL, json=parameters)
+
+add_exercise("run", 20,100)
+    
