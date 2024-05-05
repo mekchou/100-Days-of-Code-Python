d932732cbfd3a7bd5c26b0d9a8c56ec6c19e263b
Mek <mek.chou@gmail.com>
2024-05-05 15:38:12 -0400

    retrive rows

--- capstone_flight_deal/data_manager.py
+++ capstone_flight_deal/data_manager.py
@@ -1,3 +1,12 @@
+import requests
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self) -> None:
+        self.sheet_url = "https://api.sheety.co/f57eb1dd3b398c09d11ae905bda32e17/flightDeals/prices"
+        
+    def retrieve_rows(self):
+        response = requests.get(url = self.sheet_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
