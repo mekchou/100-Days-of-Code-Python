293aaa9767534450ba15e3230ad641f9a9b892a0
Mek <mek.chou@gmail.com>
2024-05-05 20:32:46 -0400

    flightdata

--- capstone_flight_deal/flight_data.py
+++ capstone_flight_deal/flight_data.py
@@ -1,3 +1,9 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, departure_date, return_date) -> None:
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.destination_city = destination_city
+        self.destination_airport = destination_airport
+        self.departure_date = departure_date
+        self.return_date = return_date
