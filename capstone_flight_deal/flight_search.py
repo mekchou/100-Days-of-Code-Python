1d98ab7b8f4fbad94002eaf664fbfeee0eb49de9
Mek <mek.chou@gmail.com>
2024-05-06 16:20:17 -0400

    step 4

--- capstone_flight_deal/flight_search.py
+++ capstone_flight_deal/flight_search.py
@@ -1,10 +1,13 @@
 import requests
 import bearer
+from flight_data import FlightData
 
 AMADEUS_API_KEY = "HsjAFDneqLqrgeocM2No3jKjLIFI0bal"
 AMADEUS_API_SECRET = "W70gnyCsd9XwnbwD"
 AMADEUS_API_URL = "https://test.api.amadeus.com"
 AMADEUS_CITY_SEARCH = "/v1/reference-data/locations/cities"
+AMADEUS_FLIGHT_OFFERS = "/v2/shopping/flight-offers"
+AMADEUS_FLIGHT_DATES = "/v1/shopping/flight-dates"
 AMADEUS_TOKEN_ENDPOINT = "/v1/security/oauth2/token"
 
 class FlightSearch:
@@ -13,8 +16,8 @@ class FlightSearch:
         self.access_token = self.get_access_token()
         
         
-    def search_iata(self):
-        return "testing"
+    # def search_iata(self):
+    #     return "testing"
 
     def get_access_token(self):
         headers = {
@@ -31,9 +34,49 @@ class FlightSearch:
 
     def search_city(self, keywords):
         body = {
-                "keyword": keywords,
-                "max": 1
+            "keyword": keywords,
+            "max": 1
         }
         response = requests.get(url=f"{AMADEUS_API_URL}{AMADEUS_CITY_SEARCH}", auth=bearer.BearerAuth(self.access_token), params= body)
         data = response.json()
         return data
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        body = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time,
+            "returnDate": to_time,
+            "nonStop": "true",
+            "travelClass": "ECONOMY",
+            "adults": 1,
+            "currencyCode": "USD",
+            "max": 10,
+        }
+        response = requests.get(url=f"{AMADEUS_API_URL}{AMADEUS_FLIGHT_OFFERS}", auth=bearer.BearerAuth(self.access_token), params= body)
+        try:
+        # response.raise_for_status()
+            data = response.json()
+            price = data["data"][0]["price"]["grandTotal"]
+        except:
+            print("no flight found")
+            price = 9999
+        # print(data)
+        # departure_iata0 = data["data"][1]["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+        # price = data["data"][1]["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+        flight_data = FlightData(
+            price = price,
+            origin_city = origin_city_code,
+            origin_airport = origin_city_code,
+            destination_city = destination_city_code,
+            destination_airport = destination_city_code,
+            departure_date = from_time,
+            return_date = to_time,
+        )
+        
+        
+        
+        return flight_data
+        # price_1= data["data"][6]["price"]["grandTotal"]
+        # print(price_0)
+        # print(price_1)
