c0fb75ce0a888401aacd605e1176127036bc48e0
Mek <mek.chou@gmail.com>
2024-05-06 21:17:22 -0400

    notification manager that send email

--- capstone_flight_deal/notification_manager.py
+++ capstone_flight_deal/notification_manager.py
@@ -1,3 +1,22 @@
+import smtplib
+import os
+
+GMAIL_SMTP = "smtp.gmail.com"
+GMAIL_PORT = 587
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self) -> None:
+        self.my_email = os.environ.get("ENV_MY_EMAIL")
+        self.my_password = os.environ.get("ENV_MY_PASSWORD")
+        self.receiver = "mek.chou@outlook.com"
+
+    def send_email(self, content):
+        with smtplib.SMTP(host=GMAIL_SMTP, port=GMAIL_PORT) as connection:
+            connection.starttls()
+            connection.login(user=self.my_email, password=self.my_password)
+            connection.sendmail(
+                from_addr=self.my_email,
+                to_addrs=self.receiver,
+                msg=f"Subject: Flight Price Alert\n\n{content}",
+            )
