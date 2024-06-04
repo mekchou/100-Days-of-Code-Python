84594b8aa2c0c83bf7c434746220e35fa728ff96
mek <mek.chou@gmail.com>
2024-06-03 21:26:13 -0400

    spotify auth issue

--- D46/main.py
+++ D46/main.py
@@ -1,4 +1,30 @@
 import scraping
+import spotipy
+# from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+import os
+
+scope = "playlist-modify-private"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id="45c3f03776cd4347adf0d2cffec9e0bf", 
+        client_secret="8b107bde5c5e413f9e16419dd8dca8d8",
+        # scope=scope,
+        redirect_uri="http://127.0.0.1",
+        # show_dialog=True,
+        cache_path = "./D46/token.txt",
+        # username="mekchou"
+        )
+    )
+# print()
+results = sp.current_user()["id"]
+
+# for idx, item in enumerate(results['items']):
+    # track = item['track']
+    # print(idx, track['artists'][0]['name'], " – ", track['name'])
+# auth_manager = SpotifyClientCredentials()
+# sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
 
@@ -15,4 +41,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    pass
