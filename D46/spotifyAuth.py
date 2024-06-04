e6062b37213b4394ca30f21cffb82d0587605dd5
Mek <mek.chou@gmail.com>
2024-06-04 09:40:51 -0400

    reconfigure variables

--- D46/spotifyAuth.py
+++ D46/spotifyAuth.py
@@ -3,14 +3,20 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 
+SPOTIPY_CLIENT_ID = os.environ.get("SPOTIPY_CLIEND_ID")
+SPOTIPY_CLIENT_SECRET = os.environ.get("SPOTIPY_CLIEND_SECRET")
+SPOTIPY_REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI")
+
 scope = "playlist-modify-private"
+
+
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        client_id=os.environ.get("SPOTIPY_CLIEND_ID"), 
-        client_secret=os.environ.get("SPOTIPY_CLIEND_SECRET"),
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
         scope=scope,
-        redirect_uri=os.environ.get("SPOTIPY_REDIRECT_URI"),
+        redirect_uri=SPOTIPY_REDIRECT_URI,
         show_dialog=True,
         cache_path = "./D46/token.txt",
         username="mekchou"
