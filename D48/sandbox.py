ee9c26f6b1575d61faa924795408aec661347a0f
Mek <mek.chou@gmail.com>
2024-06-04 18:40:54 -0400

    challenge1

--- D48/sandbox.py
+++ D48/sandbox.py
@@ -1,7 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.google.com")
+driver.get("https://www.python.org/")
+
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole").text
+
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction").text
+# print(f"{price_dollar}.{price_cents}")
+
+
+# search_bar = driver.find_element(By.NAME, value="q")
+# print(search_bar.get_attribute("placeholder"))
+
+# documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# print(documentation_link)
+
+# bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+
+
+# //*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[1]/time
+# //*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[1]/a
+# //*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[2]/time
+
+upcoming_events = {}
+for n in range(5):
+    events_date = driver.find_element(By.XPATH, value=f'//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[{n+1}]/time').text
+    events_name = driver.find_element(By.XPATH, value=f'//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[{n+1}]/a').text
+    upcoming_events[n] = {
+            "time": events_date,
+            "name": events_name,
+        }
+print(upcoming_events)
+
+
+input("Press Enter to close the browser...")
