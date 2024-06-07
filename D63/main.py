2e6aedd83a6c31fc2473abc93529df46a4763750
Mek <mek.chou@gmail.com>
2024-06-07 14:19:08 -0400

    practice

---
+++ D63/main.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+db = sqlite3.connect("D63/books-collection.db")
+
+
+cursor = db.cursor()
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+
+# db.commit()
+
+
+cursor.execute("select * From books")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+db.close()
